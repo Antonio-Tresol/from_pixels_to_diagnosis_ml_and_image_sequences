050479999d67e94b9ed282950c03cdcd50620775
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2024-10-31 20:28:56 -0600

    update config and vivit experiment

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import random
 PROJECT = "cs_research"
 MODEL_NAME = "google/vivit-b-16x2-kinetics400"
 CONVNEXT_MODEL_NAME = "facebook/convnext-tiny-224"
-RUN_NAME = f"vivit_dummy_run_{random.randint(0, 9999)}"
 RUN_NAME_CONVNEXT = f"convnext_dummy_run_{random.randint(0, 9999)}"
+RUN_NAME = f"vivit_run_{random.randint(0, 9999)}"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # General constants
@@ -13,8 +13,8 @@ DATASET_DIR = "dataset"
 TEST_SIZE = 0.3
 SEED = 42
 EPOCHS = 20
-TRAIN_BATCH = 2
-EVAL_BATCH = 2
+TRAIN_BATCH = 10
+EVAL_BATCH = 10
 WEIGHT_DECAY = 0.01
 TRAINING_DIR = "/tmp/results"
 
